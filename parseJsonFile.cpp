9c57d961b6c917812b32f8eaa5160467c6a89c68
HubMarcus <a1852054@adelaide.edu.au>
2024-08-22 23:38:58 +0930

    Slight changes

--- parseJsonFile.cpp
+++ parseJsonFile.cpp
@@ -2,22 +2,20 @@
 #include <fstream>
 #include "json.hpp"
 
-using json = nlohmann::json;
-
-json parseJsonFile(const std::string& filename) {
+nlohmann::json parseJsonFile(const std::string& filename) {
     std::ifstream file(filename);
 
     if (!file.is_open()) {
         throw std::runtime_error("Unable to open " + filename);
     }
 
-    json jsonData;
+    nlohmann::json jsonData;
     file >> jsonData;
 
     return jsonData;
 }
 
 int main() {
-    json data = parseJsonFile("test.json");
+    nlohmann::json data = parseJsonFile("test.json");
     std::cout << "Parsed JSON data: " << data.dump(4) << std::endl;
 }
