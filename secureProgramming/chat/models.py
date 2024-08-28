83ee22798d075c4bb48c231b56017662aef5aa5f
Mona Lisa <a1850196@adelaide.edu.au>
2024-08-28 14:37:53 +0930

    binned

--- secureProgramming/chat/models.py
+++ secureProgramming/chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    sender =  models.CharField(max_length=255) #hash of the receiver
+
+    body = models.TextField() # the message itself
+
+    timestamp = models.DateTimeField(auto_now_add=True) # auto adds based on the time it was sent
+
+    def __str__(self):
+        return f"Message from {self.sender} - {self.body}"
