4990d7ace20c262e06808a633f1dc27219c9ae5c
HubMarcus <a1852054@adelaide.edu.au>
2024-09-15 14:02:48 +0930

    checkValidJSON function completed with many test files.

--- parseJsonFile.cpp
+++ parseJsonFile.cpp
@@ -1,21 +1,18 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
+
 #include "json.hpp"
 
 nlohmann::json parseJsonFile(const std::string& filename) {
-    std::ifstream file(filename);
+  std::ifstream file(filename);
 
-    if (!file.is_open()) {
-        throw std::runtime_error("Unable to open " + filename);
-    }
+  if (!file.is_open()) {
+    throw std::runtime_error("Unable to open " + filename);
+  }
 
-    nlohmann::json jsonData;
-    file >> jsonData;
+  nlohmann::json jsonData;
+  file >> jsonData;
 
-    return jsonData;
-}
-
-int main() {
-    nlohmann::json data = parseJsonFile("test.json");
-    std::cout << "Parsed JSON data: " << data.dump(4) << std::endl;
+  return jsonData;
 }
--- test.cpp
+++ test.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+
+#include "checkValidJSON.cpp"
 #include "json.hpp"
+#include "parseJsonFile.cpp"
 
 int main() {
-    
+  std::string file_name;
+  std::cout << "Enter the name of the JSON file to test: ";
+  std::cin >> file_name;
+
+  nlohmann::json message = parseJsonFile(file_name);
+  int last_counter = 12345;
+
+  if (validate_message(message, last_counter)) {
+    std::cout << "Message is valid." << std::endl;
+  } else {
+    std::cout << "Message is invalid." << std::endl;
+  }
+
+  return 0;
 }
