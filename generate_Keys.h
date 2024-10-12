abbd996d1283ce9f458a13fdeaa96ec039469e9d
suboklas <baileyruat@gmail.com>
2024-10-12 12:18:20 +1030

    key gen files

---
+++ generate_Keys.h
@@ -0,0 +1,12 @@
+#ifndef RSA_KEYGEN_H
+#define RSA_KEYGEN_H
+
+#include <openssl/rsa.h>
+
+// Function to generate a 2048-bit private RSA key
+RSA* generate_private_key();
+
+// Function to extract the public key from the private key
+RSA* generate_public_key(RSA* private_key);
+
+#endif // RSA_KEYGEN_H
